fb9e4450bbcd0f86c28239ce8f07b414762100a2
Abhinand.Arun <abhinand.arun@ust.com>
2024-09-09 17:18:43 +0530

    feat: Add routing in dashboard page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component:  LandingComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  // { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' } // Redirect to home by default
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
