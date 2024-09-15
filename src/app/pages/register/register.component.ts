1ffbfa34ff26743d196ebc54476ab890026f658a
Abhinand.Arun <abhinand.arun@ust.com>
2024-09-15 17:46:39 +0530

    feat: Integrate register user feature

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastService } from '../../services/toast.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private toast: ToastService,
+    private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password_confirmation: ['', Validators.required],
+      marketing_accept: [false]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      this.authService.register(this.registerForm.value).subscribe(
+        {
+          next: () => {
+            this.toast.success('Registration successful');
+            this.router.navigate(['login']);
+          },
+          error: (error) => {
+            this.toast.error(error);
+          }
+        }
+      );
+    }
+  }
 }
